fb68f097a659002ac2e40da5440e4a5e30768e19
Ariel Krakowski <arikrak@gmail.com>
2012-11-07 15:40:35 -0500

    long URLs now converted to goo.gl urls

--- script.js
+++ script.js
@@ -1,73 +1,69 @@
 chrome.browserAction.onClicked.addListener(emailTabs);
 
-function getTabs(tabs) {
-  
-  var body = ""
-  var subject = "--Tabs about: " //too messy for subject
-  var full_link="mailto:?body=";
-  var signature = "%0A --Sent with Email All Tabs :)";
-  var max_url = 1023; //2000?
-  for (var i = 0; i < tabs.length; i++) {
-    body += encodeURIComponent(tabs[i].url) + "%0A";
-    subject += makeTitle(tabs[i].title);
-  }
-  if(full_link.length + subject.length+ body.length + signature.length < max_url){
-  full_link += subject+"%0A"+body + signature;
-  }
-  else{
-    full_link += body;
-    if(full_link.length > max_url){
-    //next version can use goo.gl to shorten links
-    full_link = full_link.substring(0, max_url-16);
-    full_link = full_link.substring(0, Math.min(full_link.length, full_link.lastIndexOf("%0A"))) + "%0A ..shortened";
-   }
- }
-   
-  console.log(full_link);
-  return full_link;
-}
-
-function makeTitle(title)
-{
-  //allow option for keeping titles or not
-    max = 28;
-    if(title.length > max){
-      title = title.substring(0, max);
-      title = title.substring(0, Math.min(title.length, title.lastIndexOf(" "))) + "..";
-    }
-    return encodeURIComponent(title) +" * ";
-}
-
 function emailTabs() {
     chrome.tabs.getAllInWindow(null, function(tabs){
-        var fulldata = getTabs(tabs);
-        chrome.tabs.create({ url: fulldata });
+        getTabs(tabs, function(full_mail_link){
+          chrome.tabs.create({ url: full_mail_link });
+        });
     });
 }
 
-function getShortURL(long_url)
+function getTabs(tabs, callback) {  
+  var body = "mailto:?body=";
+  var signature = "%0A -Sent with Email All Tabs";
+  var max_ttl_url = 2000 - signature.length;
+  
+  var length = tabs.length;
+
+  var avg = max_ttl_url / length;  //can also use set num. e.g. 150. (encoded is longer..)
+
+  for(i=0;i<length;i++){
+    url = tabs[i].url;
+    if(url.length > avg){ 
+      getShortURL(url, function(short_url) {               
+         url = short_url; 
+       });       
+    }    
+    body += encodeIt(url);    
+  }
+
+    //extra check, if somehow too long
+    if(body.length > max_ttl_url){
+      body= body.substring(0, max_ttl_url) + "..." ;
+    }
+
+      body += signature; 
+      callback(body); 
+
+}
+
+
+function encodeIt(url)
 {
-var xmlHttp = new XMLHttpRequest();
-
-xmlHttp.open("POST", "https://www.googleapis.com/urlshortener/v1/url", true);
-xmlHttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-
-var req = new Object();
-req.longUrl = long_url;
-
-var jsonStr = JSON.stringify(req);
-xmlHttp.send(jsonStr);
-
-var short_url = JSON.parse(jsonStr).id ; //remove tostringandback loop
-
-//add check for success in json or string
-if(short_url!= null && short_url.substring(0,4)=="http"){
-  return short_url;
-}
-else{
-  return long_url; //**or nothing if nec.. ** 
+  return encodeURIComponent(url)+ "%0A"; 
 }
 
 
-}
+function getShortURL(long_url, callback ){
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("POST", "https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBXFUjrttHd7zWtYIbWkicMKqPgTvrybsQ", false); 
+  //false, since asynchronous creates too many complications, but keeping callbacks in.
 
+  xmlHttp.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+
+  jsonStr = JSON.stringify( {longUrl: long_url} );
+  xmlHttp.send(jsonStr);
+
+            var obj = JSON.parse(xmlHttp.response);
+            short_url = obj.id;
+            //console.log(short_url);
+
+            //if some error, just send back long URL
+            if(short_url != null && short_url.substring(0,4)=="http"){
+              callback(short_url);
+            }
+            else{
+              callback(long_url);
+            }
+            
+}
